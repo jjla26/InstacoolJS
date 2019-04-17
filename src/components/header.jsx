963b8a64b57b8523da3d274710b3f0635291b30c
Julio Lopez <julioj.lopeza@gmail.com>
2019-04-17 19:34:51 -0400

    components

---
+++ src/components/header.jsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+export default class Header extends React.Component{
+    render(){
+        return(
+            <header className="App-header">
+                {this.props.children}
+            </header>
+        )
+    }
+}
